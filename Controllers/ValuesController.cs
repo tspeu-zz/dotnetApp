04396f4d11fe1f055125a1c0c3a15088fdbd5ec3
tspeu <tenshotseuropa@gmail.com>
2018-10-17 21:45:30 +0100

    add 2

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,14 +14,14 @@ namespace DatinApp.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Hello", "World" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "value->" + id;
         }
 
         // POST api/values
