e11e1ca9707d8e9f8bb8bc9f7fe8fdb3adfa9b01
tspeu <tenshotseuropa@gmail.com>
2018-10-21 16:31:04 +0100

    8 retrieving data fron dB

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -2,30 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatinApp.API.Controllers
 {
-//http://localhost:5000/api/elNombredelConrollerValuesControllere
+    //http://localhost:5000/api/elNombredelConrollerValuesControllere
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        //CONTRUCTOR    ctrl . da opciones en .net
+        private readonly DataContext _ctx;
+        public ValuesController(DataContext ctx)
+        {
+            // this._ctx = ctx;
+            _ctx = ctx;
+
+        }
+
+        // public ActionResult<IEnumerable<string>> Get()
+        //IActionsResult devuelve ademas http response
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetData()
         {
             //mostrar execpsiones
-           // throw new Exception("test excepticion");
+            // throw new Exception("test excepticion");
 
-            return new string[] { "Hello", "World" };
+//se pasa el contexto y se devuelve Values. de tipo lista
+            var data = _ctx.Values.ToList();
+//como devuelve 200 de http se usa ok-y la data en forma de lista
+            return Ok(data);
         }
+
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // public ActionResult<string> Get(int id)
+        public ActionResult GetData(int id)
         {
-            return "value->" + id;
+            //la interface Datacontext
+            //usan 
+            var data = _ctx.Values.FirstOrDefault(x =>
+                        x.id == id );
+
+            return Ok(data);
         }
 
         // POST api/values
