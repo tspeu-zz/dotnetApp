c85d583d5cb6e2e2d5025dd2645607f607429784
tspeu <tenshotseuropa@gmail.com>
2018-10-26 19:58:57 +0100

    add dB User

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -3,14 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-public class DataContext : DbContext
- {
+    public class DataContext : DbContext
+    {
 
 //coasntruccotr cto*->
     public DataContext(DbContextOptions<DataContext> options) :base(options){}
 //cosntructor con la llamada a super() de java a la clase
 
-    public DbSet<Value> Values { get; set; }
 //la referecmai a los POJOS
-}
+    public DbSet<Value> Values { get; set; }
+
+//cada POJO lleva un DbSet
+    public DbSet<User> Users { get;  set; }
+//Se debe crear una nuva migration, es decir una tabla en el 
+//contexto usanto ef Entity Framework
+//dotnet ef migrations add AddUserEntity  dotnet ef migrations add AddedUserEntity
+    }
 }
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+         public int id { get ; set;}
+        
+        public string Username { set;  get;}
+
+        public byte[] PasswordHash {get; set;}
+
+        public byte[] PasswordSalt {get; set;}
+    }
+}    
