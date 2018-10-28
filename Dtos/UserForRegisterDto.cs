f38db355d19870d3ba446cfada9af9db1e19d532
tspeu <tenshotseuropa@gmail.com>
2018-10-28 18:08:56 +0000

    Dto add

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username {get; set;}
 
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage= "Debe tener entre 4 y 8 letras")]
         public string Password {get; set;}
     }
 }
